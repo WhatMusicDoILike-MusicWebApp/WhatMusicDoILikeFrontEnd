38809abbbd65a32e20eef8574252d2ef0b834f15
Caleb <calebagaro7@gmail.com>
2025-04-10 16:23:03 -0700

    Changes for youtube frontned

--- src/components/pages/dashboard/YoutubeContent.tsx
+++ src/components/pages/dashboard/YoutubeContent.tsx
@@ -1,14 +1,30 @@
 
 import axios from "axios";
-import { Button } from "../../ui";
+import { Button, Separator } from "../../ui";
 import { useAuth } from "@clerk/clerk-react";
+import { FetchMusicDataResponse, Playlist } from "../constants-types";
+import { BACKEND_ENDPOINT } from "@/main";
+import { PlaylistCards } from "./PlaylistCards";
+import { useState } from "react";
 
 export const YoutubeContent = (): JSX.Element => {
     const { userId } = useAuth();
+    const [playlistData, setPlaylistData] = useState<Playlist[]>([]);
+    
 
     const handleYtAuthClick = async () => {
         try {
-            const response = await axios.post('http://127.0.0.1:5000/youtube/yt_auth', { userId });
+            const response = await axios.post<Playlist[]>(`${BACKEND_ENDPOINT}/youtube/yt_auth`, {  userId } );
+            console.log(response.data);  // Log response properly
+            setPlaylistData(response.data)
+        } catch (error) {
+            console.error("Error during YouTube Auth:", error);
+        }
+    };  
+
+    const handlePlaylist = async () => {
+        try {
+            const response = await axios.post<FetchMusicDataResponse>(`${BACKEND_ENDPOINT}/youtube/yt_create_playlist`, {  userId } );
             console.log(response.data);  // Log response properly
         } catch (error) {
             console.error("Error during YouTube Auth:", error);
@@ -17,8 +33,20 @@ export const YoutubeContent = (): JSX.Element => {
 
 
     return (
-        <div>
-            <Button onClick={handleYtAuthClick}>Connect and Fetch Your Youtube</Button>
-        </div>
+        <>
+            <div className="flex flex-row items-center justify-start px-8">
+                <h1 className="text-2xl font-bold text-gray-100 pr-4">Playlists</h1>
+                    <Button onClick={handleYtAuthClick}>Connect and Fetch Your Youtube</Button>
+                <h2 className="text-2xl font-bold text-gray-100 pr-4">Create PLaylist</h2>
+                    <Button onClick={handlePlaylist}>Playlist Test</Button>
+                </div>
+
+            <div className='flex flex-col justify-center items-center w-full'>
+                <div className='w-full px-8'>
+                    <Separator className="my-4 rounded-sm" />
+                </div>
+            </div>
+            <PlaylistCards playlistData={playlistData} />
+        </>
     )
 }
